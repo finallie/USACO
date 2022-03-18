5d975dd77b69b61687878b24cb6c3d797251a712
finallie <1006492957@qq.com>
2022-03-19 02:07:07 +0800

    task ride

--- 1.2/beads.cpp
+++ 1.2/beads.cpp
@@ -14,18 +14,40 @@ int main() {
     int n;
     string s;
     fin >> n >> s;
-    vector<ptrdiff_t> v;
-    for (auto i = cbegin(s), j = cend(s); i < j;) {
-        auto i2 = find_if_not(i + 1, j, [i](auto &&t) { return t == *i; });
-        v.push_back(distance(i, i2));
-        i = i2;
+    s += s;
+    vector<int> lr(2 * n + 1), lb(2 * n + 1), rr(2 * n + 1), rb(2 * n + 1);
+    for (int i = 1; i <= 2 * n; ++i) {
+        if (s[i - 1] == 'r') {
+            lr[i] = lr[i - 1] + 1;
+            lb[i] = 0;
+        } else if (s[i - 1] == 'b') {
+            lr[i] = 0;
+            lb[i] = lb[i - 1] + 1;
+        } else {
+            lr[i] = lr[i - 1] + 1;
+            lb[i] = lb[i - 1] + 1;
+        }
     }
-
-    ptrdiff_t ans = 0;
-    for (decltype(v.size()) size = v.size(), i = 0; i < size; ++i) {
-        ans = max(v[i] + v[(i + 1) % size], ans);
+    for (int i = 2 * n - 1; i >= 0; --i) {
+        if (s[i] == 'r') {
+            rr[i] = rr[i + 1] + 1;
+            rb[i] = 0;
+        } else if (s[i] == 'b') {
+            rr[i] = 0;
+            rb[i] = rb[i + 1] + 1;
+        } else {
+            rr[i] = rr[i + 1] + 1;
+            rb[i] = rb[i + 1] + 1;
+        }
     }
+    int ans = 0;
+    for (int i = 0; i <= 2 * n; ++i) {
+        ans = max(ans, max(lr[i], lb[i]) + max(rr[i], rb[i]));
+    }
+    ans = min(n, ans);
     fout << ans << endl;
+    fin.close();
+    fout.close();
     return 0;
 }
 
