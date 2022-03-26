2982bbb70287f9b576dbfcaa4f584505bb339dbd
y <1006492957@qq.com>
2022-03-26 10:37:16 +0800

    1.5

--- 1.5/ariprog.cpp
+++ 1.5/ariprog.cpp
@@ -8,27 +8,35 @@ LANG: C++
 using namespace std;
 
 int N, M;
+int bisquares[250 * 250 * 2 + 1]{};
+bool is_bisquare[250 * 250 * 2 + 1]{};
+int num_bisquares = 0;
+
 
 int main() {
     ifstream fin("ariprog.in");
     ofstream fout("ariprog.out");
     fin >> N >> M;
-    int bisquares[250 * 250 * 2 + 1]{};
     for (int p = 0; p <= M; ++p) {
         for (int q = 0; q <= M; ++q) {
-            bisquares[p * p + q * q] = 1;
+            int t = p * p + q * q;
+            if (!is_bisquare[t]) {
+                is_bisquare[t] = true;
+                bisquares[num_bisquares++] = t;
+            }
         }
     }
+    sort(begin(bisquares), begin(bisquares) + num_bisquares);
+    const int upper = M * M * 2;
+
     bool no_answer = true;
-    for (int b = 1, max_b = M * M * 2 / (N - 1); b <= max_b; ++b) {
-        for (int a = 0; a + (N - 1) * b <= M * M * 2; ++a) {
-            int i;
-            for (i = 0; i < N; ++i) {
-                if (bisquares[a + i * b] == 0)
-                    break;
-            }
-            if (i == N) {
-                fout << a << ' ' << b << '\n';
+    for (int b = 1, max_b = upper/ (N - 1); b <= max_b; ++b) {
+        for (int i = 0; bisquares[i]+b*(N-1)<=upper; ++i) {
+            int j = 1;
+            for(;j<=N-1;++j)
+                if(!is_bisquare[bisquares[i]+j*b]) break;
+            if (j == N) {
+                fout << bisquares[i] << ' ' << b << '\n';
                 no_answer = false;
             }
         }
