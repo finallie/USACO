d89e15fb818978429907bfcd1f58470f507e966b
y <1006492957@qq.com>
2022-03-30 02:41:15 +0800

    2.1

--- 2.2/runround.cpp
+++ 2.2/runround.cpp
@@ -12,43 +12,31 @@ ifstream fin("runround.in");
 ofstream fout("runround.out");
 ll N;
 
-ll next_runround(ll n) {
-    vector<int> v;
-    while (n) {
-        v.push_back(n % 10);
-        n /= 10;
+bool is_runround(ll n) {
+    bool check[10]{true};
+    string s = to_string(n);
+    for (const auto c: s) {
+        if (check[c - '0']) return false;
+        check[c - '0'] = true;
     }
-    std::reverse(v.begin(), v.end());
-
-    while (true) {
-        bool found = false;
-        for (int i = 0; i < v.size(); ++i) {
-            int j = (i + v[i]) % v.size();
-            if (v[i] == v[j]) {
-                found = true;
-                int index = max(i, j);
-                while (v[index] == 9 && index >= 0)
-                    --index;
-                if (index == -1) {
-                    index = 0;
-                    v.insert(begin(v), 0);
-                }
-                v[index]++;
-                fill(begin(v) + index + 1, end(v), 1);
-                break;
-            }
+    int t = 0, len = s.size();
+    for (int i = 0, j; ;) {
+        j = (i + s[i] - '0') % len;
+        s[i] = 0;
+        i = j;
+        ++t;
+        if (!s[j]) {
+            return t >= len && j == 0;
         }
-        if (!found) break;
     }
-
-    for (ll i: v)
-        n = n * 10 + i;
-    return n;
 }
 
 int main() {
     fin >> N;
-    fout << next_runround(N + 1) << endl;
+    do {
+        ++N;
+    } while (!is_runround(N));
+    fout << N << endl;
     return 0;
 }
 
