40690dd93381ccd77b7740dc50460b96177b6d99
ny <niyi@@lswooks.com>
2022-04-06 18:12:49 +0800

    3.2

---
+++ 3.2/ratios.cpp
@@ -0,0 +1,18 @@
+/*
+ID: aleksil1
+TASK: ratios
+LANG: C++
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin("ratios.in");
+ofstream fout("ratios.out");
+
+
+int main() {
+
+    return 0;
+}
+
