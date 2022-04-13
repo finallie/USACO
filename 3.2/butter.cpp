6304c72bb661a4a4ab0e6e65721072a5fc34c0f5
y <1006492957@qq.com>
2022-04-14 02:02:14 +0800

    2.1

--- 3.2/butter.cpp
+++ 3.2/butter.cpp
@@ -7,37 +7,50 @@ LANG: C++
 
 using namespace std;
 
-ifstream fin("butter.in");
-ofstream fout("butter.out");
 
-int N, P, C, cow[500], path[800][800];
+int N, P, C, cow[500], dis[801][801];
+constexpr int INF = INT_MAX / 2;
 
 int main() {
+    ifstream fin("butter.in");
+    ofstream fout("butter.out");
     fin >> N >> P >> C;
     for (int i = 0; i < N; ++i) {
         fin >> cow[i];
-        cow[i]--;
+        fill_n(dis[cow[i]], P + 1, INF);
     }
-    fill_n((int *) path, 800 * 800, 225 * (P - 1));
+    vector<vector<pair<int, int>>> graph(P + 1);
     for (int i = 0, u, v, d; i < C; ++i) {
         fin >> u >> v >> d;
-        path[u - 1][v - 1] = path[v - 1][u - 1] = d;
+        graph[u].emplace_back(v, d);
+        graph[v].emplace_back(u, d);
     }
-    for (int i = 0; i < P; ++i) {
-        path[i][i] = 0;
-    }
-    for (int k = 0; k < P; ++k) {
-        for (int i = 0; i < P; ++i) {
-            for (int j = 0; j < P; ++j) {
-                path[i][j] = min(path[i][j], path[i][k] + path[k][j]);
-            }
+
+    vector<pair<int, int>> vec;
+//    vec.reserve(C * 2);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq(greater<>(),vec);
+    for (int n = 0; n < N; ++n) {
+        if (dis[cow[n]][cow[n]] != INF) continue;
+        vec.clear();
+        pq.emplace(0, cow[n]);
+        while (!pq.empty()) {
+            int v, d;
+            tie(d, v) = pq.top();
+            pq.pop();
+            if(dis[cow[n]][v]!=INF) continue;
+            dis[cow[n]][v] = d;
+            for (const auto &p : graph[v])
+                if (dis[cow[n]][p.first]==INF) {
+                    pq.emplace(d + p.second, p.first);
+                }
         }
     }
+
     int ans = INT_MAX;
-    for (int i = 0; i < P; ++i) {
+    for (int i = 1; i <= P; ++i) {
         int sum = 0;
-        for (int j = 0; j < N; ++j) {
-            sum += path[i][cow[j]];
+        for (int j = 0; j <= N; ++j) {
+            sum += dis[cow[j]][i];
         }
         ans = min(ans, sum);
     }
